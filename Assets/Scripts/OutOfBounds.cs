840b8d167a3b25c78b460a68a674eb357deca9a6
Spencer <thebuff2001@gmail.com>
2021-06-02 13:37:25 -0600

    Lives in theory done

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class OutOfBounds : MonoBehaviour
 {
     public GameObject newBall;
+    public GameObject lifeOne;
+    public GameObject lifeTwo;
+    public GameObject lifeThree;
+    public int lives = 3;
 
     private void OnCollisionEnter2D(Collision2D obj)
     {
-        
-            Destroy(obj.gameObject);
+        lives--;
+        Destroy(obj.gameObject);
 
-            GameObject newObj = Instantiate(newBall);
+        GameObject newObj = Instantiate(newBall);
             
-            newObj.transform.position = new Vector3(Random.Range(-1.0f,1.0f), 5.01f, 0f);
+        newObj.transform.position = new Vector3(Random.Range(-1.0f,1.0f), 5.01f, 0f);
+            
+        if(lives == 2)
+        {
+            Destroy(lifeOne);
+        }
+        else if(lives == 1)
+        {
+            Destroy(lifeTwo);
+        }
+        else if(lives == 0)
+        {
+            Destroy(lifeThree);
+        }
 
-        
     }
 }
