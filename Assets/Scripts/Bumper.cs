859d98bcad6bffe76c2d181eee57abb759b23d14
Corey Mundy <minusemerkat3@yahoo.com>
2021-06-03 08:23:33 -0600

    sdfdsfdsafsacx
    safdsafreqgrev

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -8,12 +8,6 @@ public class Bumper : MonoBehaviour
     public int bumperForce = 800;
     private GameObject player;
     private Rigidbody2D bumper;
-<<<<<<< HEAD
-=======
-    //public TMP_Text scoreTxt;
-
-    //public int score = 100;
->>>>>>> a5650f4d86fc6a039a014c581c5ff2fdaa9b2963
 
     public Animator animator;
 
@@ -27,12 +21,6 @@ public class Bumper : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-<<<<<<< HEAD
-=======
-        //scoreTxt.text = score.ToString();
-        //Debug.Log(score);
-        //Score.curScore += 100;
->>>>>>> a5650f4d86fc6a039a014c581c5ff2fdaa9b2963
         animator.SetTrigger("Hit");
         animator.SetTrigger("Hit2");
         animator.SetTrigger("Hit3");
